b714b9a1c4997321c72a430feb6475b3bb4a2ba6
Lander Dirix <lander.dirix@student.ucll.be>
2025-11-25 19:44:54 +0100

    removed .NET MAUI project

--- GymApp_frontend/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GymApp_frontend")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GymApp_frontend.Pages")]
